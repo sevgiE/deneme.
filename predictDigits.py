dcf8302c908798f6e7504f99da54cee95dabf418
Sevgi Eğri <101088769+sevgiE@users.noreply.github.com>
2023-01-13 01:56:15 +0300

    Update predictDigits.py

--- predictDigits.py
+++ predictDigits.py
@@ -4,13 +4,12 @@ from tensorflow import keras
 windowName = 'Bir Numara Cizin'
 img = np.zeros((512, 512, 3), np.uint8)
 cv2.namedWindow(windowName)
-# true if mouse is pressed
+
 drawing = False
-# if True, draw rectangle. Press 'm' to toggle to curve
+
 mode = True
 (ix, iy) = (-1, -1)
-# mouse callback function
-# brush function
+
 def draw_shape(event, x, y, flags, param):
     global ix, iy, drawing, mode
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -43,19 +42,19 @@ def main():
 if __name__ == "__main__":
     img = main()
     img2 = img
-    # converting Color of the image to gray
+ 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # resize the image
+
     img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
-    # convert img to float32 type
+  
     img = img.astype('float32')
     img /= 255
     img = np.reshape(img, (1, 28, 28, 1))
-    # loading the previously saved model
+  
     model1 = keras.models.load_model('model.h5')
-    # predicting the number in the image
+
     num = model1.predict_classes(img)[0]
-    # prints the number predicted
+
     print('number = ', num)
     f = np.zeros((500, 500, 3), dtype='uint8')
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -64,7 +63,7 @@ if __name__ == "__main__":
     color = (255, 255, 0)
     thickness = 2
     print(num)
-    # shows the predicted number in the image
+
     img2 = cv2.putText(img2, f'Tespit Edilen Sayı = {num}', org, font,
                        fontScale, color, thickness, cv2.LINE_AA)
     cv2.imshow('Predicted', img2)
